eee7fc828ec1f090ebfd6cab6cd55af37d520c2c
RyuuseiKang <ryuuseikang@naver.com>
2022-06-21 03:13:40 +0900

    fix: 상단에 들어갈 타이틀 구상 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from "react";
-import {ParallaxProvider} from "react-scroll-parallax";
 
 import "./assets/fonts/pretendardvariable.css";
 import "./assets/fonts/pretendardvariable-jp.css";
+
+import "./locale/i18n";
 
 import AppRouter from "./AppRouter";
 
 const App: React.FC = () => {
   return (
-      <ParallaxProvider>
-        <AppRouter />
-      </ParallaxProvider>
+    <AppRouter />
   );
 };
 
--- src/views/Resume.tsx
+++ src/views/Resume.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import {t} from "i18next";
+import { useTranslation } from "react-i18next";
+import Typewriter from 'typewriter-effect';
 import styled from "styled-components";
 
 import Coffee from "../assets/coffee.jpg";
+import Self from "../assets/self.png";
 
 const Container = styled.div`
   display: block;
@@ -11,35 +13,112 @@ const Container = styled.div`
 const Content = styled.div`
   margin: auto;
   max-width: 768px;
-  padding: 1rem;
+  padding: 100px 2rem;
 `;
 
 const Title = styled.span`
   font-weight: 700;
   font-size: 1.5rem;
+  line-height: 1.3em;
+  transition: all 0.3s ease-in-out;
 
   @media (min-width: 768px) {
     font-size: 1.8rem;
+    line-height: 1.5em;
   }
 `;
 
-const AkaName = styled(Title)`
-  font-size: 1.2rem;
+const ContentHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 0px;
+`;
+
+const ProfileIcon = styled.img`
+  border-radius: 50%;
+  width: 5em;
+  height: 5em;
+  transition: all 0.3s ease-in-out;
 
   @media (min-width: 768px) {
-    font-size: 1.4rem;
+    width: 8em;
+    height: 8em;
   }
 `;
 
-const Profile = styled.div``;
+const Profile = styled.div`
+  padding: 120px 0px;
+  display: flex;
+  flex-direction: row;
+`;
+
+
+const Introduce = styled.span`
+  flex: 1;  
+  font-size: 1rem;
+  line-height: 1.7em;
+
+  word-break: keep-all;
+
+  @media (min-width: 768px) {
+    font-size: 1.2rem;
+  }
+`;
+
+const IntroduceTitle = styled.span`
+  font-size: 1.4rem;
+  font-weight: 700;
+  padding-right: 100px;
+
+  @media (min-width: 768px) {
+    font-size: 1.6rem;
+  }
+
+`;
+
+
+const CoffeeImage = styled.img`
+padding: 20px 0px;
+  width: 100%;
+  word-break: keep-all;
+`;
+
+
 
 const Resume: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <Container>
       <Content>
-        <Title>염석현</Title>
-        <AkaName>(aka. RYU)</AkaName>
-        <Profile></Profile>
+        <ContentHeader>
+          <Title>
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter.typeString(`${t("resume.title")}`)
+                  .start();
+              }}
+              options={{
+                autoStart: true,
+                cursor: "|",
+              }}
+            />
+          </Title>
+          <ProfileIcon src={Self} alt="me" />
+        </ContentHeader>
+
+        <Profile>
+          <IntroduceTitle>{t("resume.introduce_title")}</IntroduceTitle>
+          <Introduce>
+            저는 리액트와 리액트 네이티브를 사용한 프론트엔드 개발과 장고를 사용한 백엔드 개발을 좋아하며, 항상 모두가 보기 좋은 코드를 작성하는 것을 좋아합니다.<br/>
+            함께 커피 한잔 마시면서 이야기를 하고 싶어요.
+            <CoffeeImage src={Coffee} alt="cafe latte" />
+          </Introduce>
+
+
+        </Profile>
       </Content>
     </Container>
   );
